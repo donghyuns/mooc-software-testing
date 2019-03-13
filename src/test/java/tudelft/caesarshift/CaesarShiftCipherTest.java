a1f405748d4bb8df33ecfe90222034cb0c814788
Donghyun Shin <donghyuns@gmail.com>
2019-03-13 14:45:38 +0900

    Caesar Shift Cipher practice

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,17 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class CaesarShiftCipherTest {
 
+    @ParameterizedTest
+    @CsvSource({"abc, efg", "xyz, bcd"})
+    public void testCaesarShiftCipher(String ori, String shifted) {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        String strReturn = csc.CaesarShiftCipher(ori, 4);
+        Assertions.assertEquals(shifted, strReturn);
+    }
 }
