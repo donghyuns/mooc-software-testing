7732d989f2b65d8b4dce614bd3cd6c5a5a4c81e3
Donghyun Shin <donghyuns@gmail.com>
2019-03-13 14:21:52 +0900

    gHappy practice

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,17 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
 public class GHappyTest {
+    @ParameterizedTest
+    @CsvSource({"xxggxx, true", "xxgxx, false", "xxggyygxx, false", "gg, true"})
+    public void testGHappy(String str, boolean expected) {
+        GHappy gg = new GHappy();
+        boolean result = gg.gHappy(str);
+        Assertions.assertEquals(expected, result);
+    }
 }
